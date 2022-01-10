ad351ba859106ca29563b70292d5d89ff99c26cc
Huzefa-Mustafa <huzefahdk@gmail.com>
2022-01-10 21:00:01 +0100

    base files added

--- src/com/project2/Main.java
+++ src/com/project2/Main.java
@@ -1,8 +1,35 @@
 package com.project2;
-
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
 public class Main {
+    /**
+     * Execute the methods.
+     *
+     * @param args
+     * @throws IOException
+     * @throws TimeoutException
+     */
+    public static void main(String[] args) throws IOException, TimeoutException{
+        TopicExchange.declareExchange();
+        TopicExchange.declareQueues();
+        TopicExchange.declareBindings();
 
-    public static void main(String[] args) {
-	// write your code here
+        Thread subscribe = new Thread(() -> {
+            try {
+                TopicExchange.subscribeMessage();
+            } catch (IOException | TimeoutException e) {
+                e.printStackTrace();
+            }
+        });
+
+        Thread publish = new Thread(() -> {
+            try {
+                TopicExchange.publishMessage();
+            } catch (IOException | TimeoutException e) {
+                e.printStackTrace();
+            }
+        });
+        subscribe.start();
+        publish.start();
     }
 }
